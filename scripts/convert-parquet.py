d4a4d673130c12c4af0238cf39c7d1013d0350ea
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-04 10:32:31 +1300

    Wrote a script using pyrisk to convert parquet

---
+++ scripts/convert-parquet.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+convert-parquet.py
+
+Converts the data from a parquet file and creates a duckdb file.
+"""
+
+import argparse
+import pathlib
+import sys
+
+import pyrisk
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Convert a parquet file to a duckdb file"
+    )
+    parser.add_argument(
+        "parquet_file",
+        metavar="parquet-file",
+        help="Path to the parquet file",
+    )
+    parser.add_argument(
+        "duckdb_file",
+        metavar="duckdb-file",
+        help="Path to the duckdb file",
+    )
+    parser.add_argument(
+        "-t",
+        "--table-name",
+        default="main",
+        help="Name of the table to create in the database",
+    )
+
+    args = parser.parse_args()
+
+    try:
+        # Read log configuration file
+        log_config = pyrisk.utils.io.read_toml_configuration("../config/log.toml")
+
+        # Create logger
+        script_name = str(pathlib.Path(__file__).stem)
+        log_path = log_config["log_path"]
+        logger = pyrisk.utils.logger.setup_logger(
+            script_name,
+            log_path,
+        )
+    except (TypeError, ValueError, FileNotFoundError, IsADirectoryError) as err:
+        sys.stderr.write("An error occured when trying to create the logger")
+        sys.stderr.write(repr(err))
+        exit(1)
+
+    try:
+        pyrisk.data.db.parquet_to_duckdb(
+            args.parquet_file, args.duckdb_file, args.table_name
+        )
+    except (TypeError, ValueError, FileNotFoundError, IsADirectoryError):
+        logger.exception(log_config["log_message"] + f"{script_name}")
+        sys.stderr.write(log_config["print_message"] + f"{log_path}/{script_name}.log")
+        exit(1)
+
+    except Exception:
+        logger.exception(log_config["log_message"] + f"{script_name}")
+        sys.stderr.write(
+            log_config["unexpected_message"] + f"{log_path}/{script_name}.log"
+        )
+        exit(1)
