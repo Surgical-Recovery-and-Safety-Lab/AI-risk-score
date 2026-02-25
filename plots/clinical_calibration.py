00a42d96b03cb1ed9217708fa1dcae1db07abbff
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-02-25 14:45:08 +1300

    Wrote a script to plot the calibration curves for the clinical paper

---
+++ plots/clinical_calibration.py
@@ -0,0 +1,373 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+clinical_calibration.py
+
+Plots calibration curves for the clinical paper.
+"""
+
+import argparse
+import pathlib
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes._axes import Axes
+from matplotlib.figure import Figure
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from sklearn.calibration import calibration_curve
+
+from medpipe import (
+    exception_handler,
+    extract_labels,
+    get_positive_proba,
+    load_data_from_csv,
+    load_pipeline,
+    print_message,
+    read_toml_configuration,
+    setup_logger,
+)
+from medpipe.utils.config import get_configuration, get_file_path, split_version_number
+from medpipe.utils.exceptions import file_checks
+
+LABEL_MAP = {
+    "ALL": "All",
+    "MORTALITY_30D": "30-day mortality",
+    "MORTALITY_90D": "90-day mortality",
+    "MORTALITY_1Y": "1-year mortality",
+    "READMIT_ACUTE_30D": "30-day acute readmission",
+    "READMIT_ACUTE_90D": "90-day acute readmission",
+    "ANY_COMP": "Any complications",
+    "SSI": "SSI",
+    "VTE": "VTE",
+    "CARDIAC_ARREST": "Cardiac arrest",
+    "SEPSIS": "Sepsis",
+    "RESPIRATORY_FAILURE": "Respiratory failure",
+    "SHOCK": "Shock",
+    "STROKE": "Stroke",
+    "AKI": "AKI",
+    "CARDIAC_ARRHYTHMIA": "Cardiac arrhythmia",
+    "DELIRIUM": "Delirium",
+    "GI_BLEEDING": "GI bleeding",
+    "HAEMORRHAGE": "Haemorrhage",
+    "IMPLANT_GRAFT": "Implant / graft complications",
+    "MYOCARDIAL_INFARCTION": "Myocardial infarction",
+    "PNEUMONIA": "Pneumonia",
+    "UTI": "UTI",
+}
+
+
+def plot_clinical_calibration(
+    y_test,
+    proba_list,
+    label_list=[],
+    distribution=False,
+    n_bootstraps=200,
+    save_path="",
+    extension=".png",
+    show_fig=True,
+    calibration_kwargs={},
+    **kwargs,
+):
+    """
+    Plots the reliability diagrams for the given probabilities.
+
+    The 95% confidence interval is calculated using the bootstrap method for
+    the calibration curve.
+    The probability distribution can also be plotted under the calibration
+    curve.
+
+    Parameters
+    ----------
+    y_test : array-like of shape (n_samples, n_classes)
+        Ground truth labels.
+    proba_list : list[array]
+        List of predicted probabilities.
+    label_list : list[str], default: []
+        List of labels for the legend.
+    distribution : bool, default: False
+        Flag to plot the probability distribution as well.
+    n_bootstraps : int, default: 200
+        Number of iteration for the bootstrap.
+    save_path : str, default: []
+        Path to the save file.
+    extension : str, default: ".png"
+        Extension to save figure in.
+    show_fig : bool, default: True
+        Flag to show the figure.
+    calibration_kwargs : dict[str, value], default: {}
+        Extra arguments for the calibration function.
+    **kwargs
+        Extra arguments for the figure or axes objects.
+
+    Returns
+    -------
+    None
+        Nothing is returned.
+
+    """
+    colours = ["#2D90D8", "#33367A", "#96690E", "#CDB4DB", "#F2CC8F"]
+
+    # Split arguments based on where they should be sent
+    ax_kwargs = {key: value for key, value in kwargs.items() if key in dir(Axes)}
+    fig_kwargs = {key: value for key, value in kwargs.items() if key in dir(Figure)}
+
+    # Set figure properties
+    fig, ax = plt.subplots(**fig_kwargs)
+
+    # Set title
+    title = kwargs["set_title"] if "set_title" in kwargs.keys() else ""
+    ax.set_title(title, fontweight="bold")
+    ax_kwargs.pop("set_title")
+
+    # Remove spines for aesthetics
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    # Plot perfect calibration
+    ax.plot(
+        np.linspace(0, 1, 100),
+        np.linspace(0, 1, 100),
+        "k--",
+        label="Perfectly calibrated",
+    )
+
+    for i in range(len(proba_list)):
+        prob_true, prob_pred = calibration_curve(
+            y_test,
+            proba_list[i],
+            **calibration_kwargs,
+        )
+
+        boots = []
+        for _ in range(n_bootstraps):
+            idx = np.random.choice(len(y_test), len(y_test), replace=True)
+            prob_true_boot, _ = calibration_curve(
+                y_test[idx],
+                proba_list[i][idx],
+                **calibration_kwargs,
+            )
+            if len(prob_true_boot) == len(prob_true):
+                boots.append(prob_true_boot)
+
+            lower = np.percentile(boots, 2.5, axis=0)
+            upper = np.percentile(boots, 97.5, axis=0)
+
+        ax.plot(
+            prob_pred,
+            prob_true,
+            marker=".",
+            color=colours[i],
+            label=label_list[i],
+        )
+        ax.fill_between(
+            prob_pred,
+            lower,
+            upper,
+            color=colours[i],
+            alpha=0.5,
+            label=f"{label_list[i]} 95% CI",
+        )
+
+        if max(prob_pred) < 0.4:
+            # Remove spines for aesthetics
+            plt.gca().spines["top"].set_visible(False)
+            plt.gca().spines["right"].set_visible(False)
+
+            ax_ins = ax.inset_axes(
+                [0.5, 0.1, 0.5, 0.5],
+                yticklabels=[],
+            )
+            ax_ins.plot(
+                prob_pred,
+                prob_true,
+                marker=".",
+                color=colours[i],
+            )
+            ax_ins.fill_between(
+                prob_pred,
+                lower,
+                upper,
+                color=colours[i],
+                alpha=0.5,
+            )
+            ax_ins.plot(
+                np.linspace(0, max(prob_pred), 100),
+                np.linspace(0, max(prob_pred), 100),
+                "k--",
+            )
+
+            # Connect the inset to the zoomed area in the main plot
+            ax.indicate_inset_zoom(ax_ins, edgecolor="black")
+
+    if distribution:
+        # Create new plot for distribution
+        divider = make_axes_locatable(ax)
+        ax_dist = divider.append_axes("bottom", 0.5, pad=0.1)
+
+        if "n_bins" in calibration_kwargs.keys():
+            bins = np.linspace(0, 1, calibration_kwargs["n_bins"] + 1)
+        else:
+            bins = np.linspace(0, 1, 21)
+
+        ax_dist.hist(
+            proba_list,
+            stacked=True,
+            color=colours[: len(proba_list)],
+            edgecolor="black",
+            bins=bins,
+            label=label_list,
+        )
+        ax_dist.set_yscale("log")
+        ax_dist.set_ylim(top=10e6)
+        ax_dist.set_yticks([10e3, 10e7])
+        ax_dist.set_yticklabels([r"$10^3$", r"$10^7$"])
+        ax_dist.set_xlabel("Predicted probabilities", fontweight="bold")
+
+        # Remove spines for aesthetics for distribution
+        plt.gca().spines["top"].set_visible(False)
+        plt.gca().spines["right"].set_visible(False)
+
+    ax.set_xlabel("Predicted probabilities", fontweight="bold")
+    ax.set_ylabel("Observed proportion", fontweight="bold")
+
+    # Set ax_kwargs to override if needed
+    for key, val in ax_kwargs.items():
+        getattr(ax, key)(val)
+
+    ax.legend(loc="upper right", bbox_to_anchor=(1.6, 0.9), title="Models")
+
+    fig.subplots_adjust(right=0.66, bottom=0.14)
+
+    if save_path:
+        save_file = save_path + extension
+        file_checks(save_file, extension=extension, exists=False)
+        plt.savefig(save_file)
+    if show_fig:
+        plt.show()
+
+    plt.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create and train a Pipeline of machine learning models"
+    )
+    parser.add_argument(
+        "model_config_file",
+        metavar="model-config-file",
+        help="Path to the general configuration file",
+    )
+    parser.add_argument("--version", help="Version number overload for multiprocessing")
+    parser.add_argument(
+        "--no-plots", action="store_false", help="Flag used to turn off plotting"
+    )
+
+    args = parser.parse_args()
+
+    try:
+        print_message("Loading parameters from configuration file")
+        # Read log and general configuration file
+        log_config = read_toml_configuration("../config/log.toml")
+        general_config = read_toml_configuration(args.model_config_file)
+
+        if args.version:
+            # Swap version numbers if overloading
+            general_config["version"] = args.version
+
+        print_message("Setting up logger")
+        # Create logger
+        script_name = str(pathlib.Path(__file__).stem)
+        script_name += "_" + general_config["version"]
+        log_dir = log_config["base_dir"] + log_config["log_dir"]
+        log_dir = log_config["log_dir"]
+        logger = setup_logger(script_name, log_dir)
+
+        print_message(
+            f"Version number: {general_config["version"]}", logger, script_name
+        )
+
+    except (TypeError, ValueError, FileNotFoundError, IsADirectoryError) as err:
+        sys.stderr.write("An error occured when trying to create the logger\n")
+        sys.stderr.write(repr(err))
+        exit(1)
+
+    try:
+        data_version, _ = split_version_number(general_config["version"])
+
+        # Get data configuration parameters
+        data_config = get_configuration(
+            general_config["data_parameters"],
+            data_version,
+        )
+
+    except Exception:
+        exception_handler(logger, log_dir, log_config, script_name)
+        exit(1)
+
+    try:
+        print_message("Getting data", logger, script_name)
+        data = load_data_from_csv(
+            get_file_path(
+                data_config, v_number=data_version[:4]  # Use only first 2 numbers
+            )
+        )
+        # Load model
+        print_message("Loading model", logger, script_name)
+        load_file = get_file_path(
+            general_config,
+            v_number=general_config["version"],
+        )
+        pipeline = load_pipeline(load_file)
+
+        data = pipeline.preprocessor.transform(data)
+        X_train, X_test = pipeline.get_test_data(data, test_group_vals=[2024])
+
+    except Exception:
+        exception_handler(logger, log_dir, log_config, script_name)
+        exit(1)
+
+    try:
+        print_message("Preparing test set", logger, script_name)
+        X_test, y_test = extract_labels(X_test, pipeline.label_list)
+        X_train, y_train = extract_labels(X_train, pipeline.label_list)
+
+    except Exception:
+        exception_handler(logger, log_dir, log_config, script_name)
+        exit(1)
+
+    # Compute statistics and plots
+    try:
+        print_message("Computing model statistics", logger, script_name)
+        group_name = data_config["split_variables"]["group_name"]
+        save_file = get_file_path(
+            general_config,
+            v_number=general_config["version"],
+            path_type="fig",
+            exists=False,
+        )
+        extension = general_config["fig_parameters"]["extension"]
+        label_list = ["Recalibrated"]
+
+        for i, label in enumerate(pipeline.label_list):
+            print_message(label, logger, script_name)
+            y_pred_proba = pipeline.predict_proba(
+                X_test, label_list=label, model_type="calibrator"
+            )
+            plot_clinical_calibration(
+                y_test[:, i],
+                [get_positive_proba(y_pred_proba).squeeze()],
+                label_list=label_list,
+                distribution=True,
+                save_path=save_file + f"_{label}_recalib_reliability_diagram",
+                extension=extension,
+                show_fig=args.no_plots,
+                calibration_kwargs={"n_bins": 10, "strategy": "quantile"},
+                dpi=300,
+                figsize=(5, 5),
+                set_title=f"{LABEL_MAP[label]}",
+            )
+
+    except Exception:
+        exception_handler(logger, log_dir, log_config, script_name)
+        exit(1)
