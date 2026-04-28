cada0095280eabe4c7e646250a1b2b7785f86ff6
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-04-29 10:55:44 +1200

    Made the base natural distribution model the comparison source

--- src/methods-paper/test_scenario.py
+++ src/methods-paper/test_scenario.py
@@ -21,12 +21,13 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    orig_version = "v0.1.1.1-a.1.2.2"  # Base model to compare to
     versions = [
-        "v0.1.1.1-a.1.2.2",
-        "v0.1.1.1-a.2.2.2",
-        "v0.1.1.1-a.10.2.2",
-        "v0.1.1.1-a.6.2.2",
-        "v0.1.1.1-a.14.2.2",
+        "v0.1.1.1-a.1.2.1",
+        "v0.1.1.1-a.2.2.1",
+        "v0.1.1.1-a.10.2.1",
+        "v0.1.1.1-a.6.2.1",
+        "v0.1.1.1-a.14.2.1",
     ]
     thresholds = [0.02, 0.1]
 
@@ -39,50 +40,58 @@ if __name__ == "__main__":
     outcome = ["MORTALITY_90D", "ANY_COMP"]
     methods = ["Natural", "CSL", "SMOTE", "ROS", "RUS"]
 
-    for k, version in enumerate(versions):
-        # Swap version numbers if overloading
-        general_config["version"] = version
-        print_message(
-            f"Version number: {general_config["version"]}", logger, script_name
-        )
-        data_version, _ = split_version_number(general_config["version"])
+    # Swap version numbers if overloading
+    print_message(f"Version number: {general_config["version"]}", logger, script_name)
+    data_version, _ = split_version_number(general_config["version"])
 
-        # Get data configuration parameters
-        data_config = get_configuration(
-            general_config["data_parameters"],
-            data_version,
+    # Get data configuration parameters
+    data_config = get_configuration(
+        general_config["data_parameters"],
+        data_version,
+    )
+    print_message("Getting data", logger, script_name)
+    data = load_data_from_csv(
+        get_file_path(
+            data_config,
+            v_number=data_version[:4],  # Use only first 2 numbers
         )
-        print_message("Getting data", logger, script_name)
-        data = load_data_from_csv(
-            get_file_path(
-                data_config,
-                v_number=data_version[:4],  # Use only first 2 numbers
-            )
-        )
+    )
+    print_message(f"Loading model {orig_version}", logger, script_name)
+    load_file = get_file_path(
+        general_config,
+        v_number=orig_version,
+    )
+    pipeline = load_pipeline(load_file)
+    _, X_test = pipeline.get_test_data(pipeline.transform(data), [2024])
+    X_test, y_test = extract_labels(X_test, pipeline.label_list)
+
+    orig_decisions = np.zeros(y_test.shape)
+    for i in range(2):
+        y_pred_proba = get_positive_proba(
+            pipeline.predict_proba(X_test, outcome[i], "predictor")
+        ).squeeze()
+        orig_decisions[:, i] = np.array(y_pred_proba > thresholds[i])
+
+    for k, version in enumerate(versions):
         # Load model
-        print_message("Loading model", logger, script_name)
+        print_message(f"Loading model {version}", logger, script_name)
         load_file = get_file_path(
             general_config,
-            v_number=general_config["version"],
+            v_number=version,
         )
         pipeline = load_pipeline(load_file)
-        _, X_test = pipeline.get_test_data(pipeline.transform(data), [2024])
-        X_test, y_test = extract_labels(X_test, pipeline.label_list)
 
-        y_pred_proba = np.zeros(y_test.shape)
         decisions = np.zeros(y_test.shape)
 
         for i in range(2):
-            y_pred_proba[:, i] = get_positive_proba(
-                pipeline.predict_proba(X_test, outcome[i], "predictor")
+            y_pred_proba = get_positive_proba(
+                pipeline.predict_proba(X_test, outcome[i], "calibrator")
             ).squeeze()
-            decisions[:, i] = np.array(y_pred_proba[:, i] > thresholds[i])
+            decisions[:, i] = np.array(y_pred_proba > thresholds[i])
 
-            if k == 0:
-                orig_decisions = decisions
             print(
                 f"{methods[k]} {outcome[i]} % risky surgeries: {100*np.sum(decisions[:, i])/len(decisions):.1f}"
             )
             print(
-                rf"{methods[k]} {outcome[i]} $\Delta$% risky surgeries: {100*(np.sum(decisions[:, i])-np.sum(orig_decisions[:, i]))/len(decisions):.1f}"
+                f"{methods[k]} {outcome[i]} Delta% risky surgeries: {100*(np.sum(decisions[:, i])-np.sum(orig_decisions[:, i]))/len(decisions):.1f}"
             )
